6c46a3b0471b8d0cb54edd45f3e153e25779b766
RitikVir <ritik.virmani@geminisolutions.in>
2019-04-01 14:16:16 +0530

    asn

--- controller/client/client.controller.js
+++ controller/client/client.controller.js
@@ -110,27 +110,26 @@ module.exports = {
       ) {
         console.log(err, checksum);
         var txn_url =
-          'https://securegw-stage.paytm.in/theia/processTransaction';
+          'https://securegw-stage.paytm.in/theia/processTransaction'; // for staging
+        // var txn_url = "https://securegw.paytm.in/theia/processTransaction"; // for production
+
         var form_fields = '';
         for (var x in params) {
           form_fields +=
-            "<input type='hidden' name='" + x + "' value='" + params[x] + "'> ";
+            "<input type='hidden' name='" + x + "' value='" + params[x] + "' >";
         }
         form_fields +=
           "<input type='hidden' name='CHECKSUMHASH' value='" + checksum + "' >";
 
-        // res.writeHead(200, { 'Content-Type': 'text/html' });
-        let formString =
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(
           '<html><head><title>Merchant Checkout Page</title></head><body><center><h1>Please do not refresh this page...</h1></center><form method="post" action="' +
-          txn_url +
-          '" name="f1">' +
-          form_fields +
-          '</form><script type="text/javascript">document.f1.submit();</script></body></html>';
-        // res.write(
-
-        // );
-        // res.end();
-        res.json({ formString });
+            txn_url +
+            '" name="f1">' +
+            form_fields +
+            '</form><script type="text/javascript">document.f1.submit();</script></body></html>'
+        );
+        res.end();
       });
     });
   },
