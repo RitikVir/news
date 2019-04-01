59a153f4875c4431f63f9ffd7d60bc43807e4290
RitikVir <ritik.virmani@geminisolutions.in>
2019-04-01 14:49:48 +0530

    jnew

--- controller/client/client.controller.js
+++ controller/client/client.controller.js
@@ -127,9 +127,8 @@ module.exports = {
           '" name="f1">' +
           form_fields +
           '</form><script type="text/javascript">document.f1.submit();</script></body></html>';
-        res.write();
+        res.write(formString);
         res.end();
-        res.json({ formString });
       });
     });
   },
